503e7f7536ba236350d23ea1b8c070d93be9641d
Chen Zhang <chen.zhang@jet.com>
2018-10-24 17:29:45 -0400

    working with mock OrderTracking

--- app/src/main/java/spike/chen/graphqlspike/MainActivity.kt
+++ app/src/main/java/spike/chen/graphqlspike/MainActivity.kt
@@ -2,11 +2,43 @@ package spike.chen.graphqlspike
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.ApolloCallback
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import com.apollographql.apollo.fetcher.ApolloResponseFetchers
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
 
 class MainActivity : AppCompatActivity() {
+
+  private var uiHandler = Handler(Looper.getMainLooper())
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val orderTrackingQuery = OrderTrackingQuery.builder().build()
+    val apolloClient = ApolloClient.builder()
+      .serverUrl("http://batman-graphapi.nomad.eastus2.qa.jet.network/graphql")
+      .okHttpClient(OkHttpClient())
+      .build()
+
+    apolloClient.query(orderTrackingQuery)
+      .responseFetcher(ApolloResponseFetchers.NETWORK_ONLY)
+      .enqueue(ApolloCallback(object : ApolloCall.Callback<OrderTrackingQuery.Data>() {
+        override fun onFailure(e: ApolloException) {
+          Log.e("Main", e.message, e)
+        }
+
+        override fun onResponse(response: Response<OrderTrackingQuery.Data>) {
+          mainText.text = response.data()?.user.toString()
+        }
+      }, uiHandler))
+
   }
 }
